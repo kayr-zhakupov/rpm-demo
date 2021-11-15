e05562b90080bd01cc7c995f1b10737acfcd5d9e
kayr.zhakupov <kayr.zhakupov@gmail.com>
2021-11-15 13:53:29 +0600

    readme update

--- scripts/db-wipe.php
+++ scripts/db-wipe.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../boot/app.php';
 
-$sql = "DROP TABLE vk_access_tokens;";
+$sql = "DROP TABLE vk_access_tokens;DROP TABLE tags;DROP TABLE tags_with_users;";
 
 app()->db()->statement($sql)->execute();
 
