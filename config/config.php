29689857f34daa98eae26dd650bec2c504d7595c
kayr.zhakupov <kayr.zhakupov@gmail.com>
2021-11-15 14:34:56 +0600

    config update

--- config/config.php
+++ config/config.php
@@ -1,15 +1,25 @@
 <?php
 
 return [
-  'friends_slice_count_initial' => 2,
-  'friends_slice_count_next' => 2,
+  /**
+   * Количество профилей, подгружаемых изначально.
+   */
+  'friends_slice_count_initial' => 10,
+
+  /**
+   * Количество профилей, подгружаемых далее.
+   */
+  'friends_slice_count_next' => 10,
 
   /**
    * Для тестирования lazy-load при малом количестве результатов.
-   * При значении unset высота составляет 117px
+   * При значении unset высота составляет 116px.
    */
 //  'friend_tile_height' => 'unset',
 //  'friend_tile_height' => '500px',
 
+  /**
+   * Threshold в px для подгрузки.
+   */
   'load_more_offset' => 128,
 ];
--- views/pages/auth/index.php
+++ views/pages/auth/index.php
@@ -10,7 +10,9 @@ $vkAuth = new VkAuth();
 ?>
 <?= view_html('core/head') ?>
 <body>
+<div style="display: flex;flex-direction: column;align-items: center;justify-content: center;height: 100%;">
 
 <?= $vkAuth->renderSignButton() ?>
 
+</div>
 </body>
